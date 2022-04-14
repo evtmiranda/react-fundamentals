a6e8cc638e35d11a68912b59658402201223b15f
Everton Miranda de Oliveira <evt.miranda@gmail.com>
2022-04-13 21:45:58 -0300

    add Card componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
+import './App.css'
+
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import Aleatorio from './components/basicos/Aleatorio';
+import Card from './components/layout/Card';
 
 const app = () => (
-    <div id='app'>
+    <div className='App' >
         <h1>Fundamentos React (Arrow)</h1>
-        <Aleatorio min={1} max={60} />
-        <Primeiro />
-        <ComParametro titulo='Segundo componente' aluno='Everton' nota={6.6} />
-        <ComParametro titulo='Segundo componente' aluno='Mariana' nota={9.1} />
-        <ComParametro titulo='Segundo componente' aluno='Gabriela' nota={9.3} />
-        <Fragmento />
-    </div>
+        <div className='Cards'>
+            <Card titulo="Número Aleatório" color="#001E6C">
+                <Aleatorio min={1} max={60} />
+            </Card>
+            <Card titulo="Fragmento" color="#E8630A">
+                <Fragmento />
+            </Card>
+
+            <Card titulo="Com Parâmetro" color="#035397">
+                <ComParametro titulo='Segundo componente' aluno='Mariana' nota={9.1} />
+            </Card>
+
+            <Card titulo="Primeiro" color="#FCD900">
+                <Primeiro />
+            </Card>
+        </div>
+    </div >
 )
 
 export default app;
